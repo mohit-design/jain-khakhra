7fa520fed5d6e398bef288231aa9958ccafa394f
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2023-05-20 17:38:29 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function() {
-
+  // Mobile Menu JS
+  $(".hamburger-menu").click(function() {
+    $("body").toggleClass("active");
+    $(this).toggleClass("active");
+    $(".navbar-menu-mobile").toggleClass("active");
+    $(".menu-listing-mobile").toggleClass("active");
+  });
 });
